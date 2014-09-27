2c32f078660a2c099d7b8e7ee9843b40d53473c8
Chad Eddington <cdavideddington@gmail.com>
2014-09-27 14:48:10 -0600

    Home page creation

---
+++ assignments/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Hello World</title>
+  <link rel="stylesheet" type="text/css" href="../style.css">
+
+
+</head>
+<body>
+
+
+	<div class="titleBar">
+		<ul>
+			<li>Chad Eddington</li>
+			<li><a href="/rhcloud/index.php">Home</a></li>
+			<li><a href="assignments/index.php">Assignments</a></li>
+		</ul>
+	</div>
+
+	<div class="page">
+
+		<h1>Assignments</h1>
+		<p> <span>Comming Soon! </span></p>
+	</div>
+
+
+
+
+
+</body>
+</html>
