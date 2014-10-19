73ef21932ff0b00156e64f836952b0ef9114497c
Chad Eddington <cdavideddington@gmail.com>
2014-10-18 23:13:25 -0600

    db take 7

--- model/db_functions.php
+++ model/db_functions.php
@@ -9,7 +9,7 @@ $dbUser = 'guest';
 $password = 'cangetin';
 
 try {
-  $mydb = new PDO("mysql:host=$dbHost:$dbPort", "adminQ7UQbmK", "hWmFxHJjIqAv");
+  $mydb = new PDO("mysql:host=$dbHost:$dbPort;dbname=moneydb", $dbUser, $password);
 } catch (PDOException $e) {
   $error_message = $e->getMessage();
   echo "An error occured while trying to connect to the database. $error_message";
